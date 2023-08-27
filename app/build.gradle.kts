f9200abb3565b4f30573c06f60c07224dcc2e530
Miguel Sánchez <msd117c@users.noreply.github.com>
2023-08-27 19:26:17 +0200

    Add versioning file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,20 @@
+import java.util.Properties
+
 plugins {
     kotlin(Plugins.kapt)
     id(Plugins.androidApplication)
     id(Plugins.kotlinAndroid)
     id(Plugins.daggerHiltAndroid)
 }
+
+val properties = Properties()
+val propertiesFile = File("version")
+properties.load(propertiesFile.inputStream())
+
+val major = (properties["version.major"] as String).toInt()
+val minor = (properties["version.minor"] as String).toInt()
+val patch = (properties["version.patch"] as String).toInt()
+val build = (properties["version.build"] as String).toInt()
 
 android {
     namespace = Configuration.namespace
@@ -13,8 +24,8 @@ android {
         applicationId = Configuration.namespace
         minSdk = Configuration.minSdk
         targetSdk = Configuration.targetSdk
-        versionCode = Configuration.versionCode
-        versionName = Configuration.versionName
+        versionCode = major.times(10000) + minor.times(1000) + patch.times(100) + build
+        versionName = "$major.$minor.$patch"
 
         testInstrumentationRunner = Configuration.testInstrumentationRunner
         vectorDrawables {
