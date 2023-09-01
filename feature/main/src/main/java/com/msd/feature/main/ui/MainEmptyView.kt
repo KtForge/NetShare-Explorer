112e0b30464dd2444c9a5d29c86761a81d3c9575
Miguel Sánchez <miguel.sanchez@xing.com>
2023-09-01 10:27:17 +0200

    Fix card size

--- feature/main/src/main/java/com/msd/feature/main/ui/MainEmptyView.kt
+++ feature/main/src/main/java/com/msd/feature/main/ui/MainEmptyView.kt
@@ -2,6 +2,7 @@ package com.msd.feature.main.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedButton
@@ -25,7 +26,9 @@ fun MainEmptyView(userInteractions: UserInteractions) {
         Card {
             Text(
                 text = stringResource(id = R.string.empty_network_configurations),
-                modifier = Modifier.padding(sizeXL)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(sizeXL)
             )
         }
 
