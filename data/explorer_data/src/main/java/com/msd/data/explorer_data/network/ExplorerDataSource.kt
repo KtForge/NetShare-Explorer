1d43757293cf52d8861331042251bef94d91f6cf
Miguel Sánchez <msd117c@users.noreply.github.com>
2023-09-11 18:53:32 +0200

    Fix issue when opening files

--- data/explorer_data/src/main/java/com/msd/data/explorer_data/network/ExplorerDataSource.kt
+++ data/explorer_data/src/main/java/com/msd/data/explorer_data/network/ExplorerDataSource.kt
@@ -3,6 +3,7 @@ package com.msd.data.explorer_data.network
 import android.content.Context
 import com.hierynomus.msdtyp.AccessMask
 import com.hierynomus.mserref.NtStatus
+import com.hierynomus.msfscc.fileinformation.FileStandardInformation
 import com.hierynomus.mssmb2.SMB2CreateDisposition
 import com.hierynomus.mssmb2.SMB2ShareAccess
 import com.hierynomus.mssmb2.SMBApiException
@@ -70,6 +71,8 @@ class ExplorerDataSource @Inject constructor(
             }
         } catch (e: Exception) {
             throw handleException(e)
+        } finally {
+            client.close()
         }
     }
 
@@ -115,7 +118,7 @@ class ExplorerDataSource @Inject constructor(
                     file.inputStream.copyTo(newFile.outputStream())
 
                     val openTime = System.currentTimeMillis() - start
-                    val fileSize = file.fileInformation.standardInformation.endOfFile
+                    val fileSize = file.getFileInformation(FileStandardInformation::class.java).endOfFile
 
                     explorerTracker.logOpenFileEvent(fileSize, openTime)
 
@@ -124,6 +127,8 @@ class ExplorerDataSource @Inject constructor(
             }
         } catch (e: Exception) {
             throw handleException(e)
+        } finally {
+            client.close()
         }
     }
 
