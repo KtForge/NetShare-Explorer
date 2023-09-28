5018a044145bde5f677fb90b1cd5d4d35a87d8a1
Miguel Sánchez <msd117c@users.noreply.github.com>
2023-09-28 20:18:01 +0200

    Update actions file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-// Change something to trigger Github Action
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") apply false
