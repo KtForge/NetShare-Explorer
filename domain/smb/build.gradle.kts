fa721a9a24c60305a3e6e6518ace255f906e90fd
Miguel Sánchez <msd117c@users.noreply.github.com>
2023-09-29 17:00:48 +0200

    Update reporting steps

--- domain/smb/build.gradle.kts
+++ domain/smb/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id(Plugins.javaLibrary)
     id(Plugins.kotlinJvm)
+    jacoco
 }
 
 java {
@@ -14,4 +15,15 @@ dependencies {
     implementation(Dependencies.inject)
 
     testImplementation(project(":core:unittest"))
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+    dependsOn(tasks.test)
 }
