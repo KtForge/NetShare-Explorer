458875891b09669ce36ce5e96d3300650266cba2
Miguel Sánchez <msd117c@users.noreply.github.com>
2024-02-24 12:51:48 +0100

    Implement reusable workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") apply false
-    id("com.android.library") apply false
-    id("org.jetbrains.kotlin.android") apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
-    id("org.jetbrains.kotlin.jvm") apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
     kotlin("plugin.serialization") version "1.9.10"
     jacoco
